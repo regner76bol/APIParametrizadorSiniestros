b0c1a1a516dfad3802485a5c903ab79abfe09e1e
regner.bernal@segurosbolivar.com-desarrolladorTI <120602279+regner76bol@users.noreply.github.com>
2024-12-25 19:56:56 -0500

    ajustes en consecuencias y en reservas

---
+++ src/main/java/com/segurosbolivar/siniestros/parametrizador/rvaautomatica/services/procedures/EditarRvaAutomaticaServiceInterface.java
@@ -0,0 +1,2 @@
+package com.segurosbolivar.siniestros.parametrizador.rvaautomatica.services.procedures;public interface EditarRvaAutomaticaServiceInterface {
+}
---
+++ src/main/java/com/segurosbolivar/siniestros/parametrizador/rvaautomatica/services/procedures/impl/EditarRvaAutomaticaServiceImpl.java
@@ -0,0 +1,2 @@
+package com.segurosbolivar.siniestros.parametrizador.rvaautomatica.services.procedures.impl;public class EditarRvaAutomaticaServiceImpl {
+}
